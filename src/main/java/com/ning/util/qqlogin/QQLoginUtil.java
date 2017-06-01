af9c92d5a1ccfb3ca530ae70e54042dd72dfd1d6
ning <1837634447@qq.com>
2017-06-01 09:44:31 +0800

    V:1.1.3(Bulid:20170601)

--- src/main/java/com/ning/util/qqlogin/QQLoginUtil.java
+++ src/main/java/com/ning/util/qqlogin/QQLoginUtil.java
@@ -13,24 +13,13 @@ import javax.servlet.http.HttpServletRequest;
  */
 public class QQLoginUtil {
     public static String getUserOpenID(HttpServletRequest request) throws LoginException {
-        String accessToken = null, openID = null;
+        String openID = null;
         try {
             AccessToken accessTokenObj = (new Oauth()).getAccessTokenByRequest(request);
-            long tokenExpireIn = 0L;
             if (accessTokenObj.getAccessToken().equals("")) {
-//                我们的网站被CSRF攻击了或者用户取消了授权
-//                做一些数据统计工作
-                System.out.print("没有获取到响应参数");
+                throw new LoginException("未获取到参数！");
             } else {
-                accessToken = accessTokenObj.getAccessToken();
-                tokenExpireIn = accessTokenObj.getExpireIn();
-                request.getSession().setAttribute("demo_access_token", accessToken);
-                request.getSession().setAttribute("demo_token_expirein", String.valueOf(tokenExpireIn));
-                // 利用获取到的accessToken 去获取当前用的openid -------- start
-                OpenID openIDObj = new OpenID(accessToken);
-                openID = openIDObj.getUserOpenID();
-                System.out.println(openID);
-                request.getSession().setAttribute("demo_openid", openID);
+                openID = new OpenID(accessTokenObj.getAccessToken()).getUserOpenID();
             }
         } catch (QQConnectException ignored) {
             throw new LoginException(ignored.getMessage());
