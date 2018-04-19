3d346e49ef2797a50234261d59b9e57cfe76a6b3
376512291@qq.com <376512291@qq.com>
2018-04-19 09:24:43 +0800

    logindao

--- src/main/java/com/ning/login/dao/UserDao.java
+++ src/main/java/com/ning/login/dao/UserDao.java
@@ -6,32 +6,75 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Created by wangn on 2017/5/19.
+ * @author wangn
+ * @date 2017/5/19
  */
 public interface UserDao {
-    /*根据用户名获取密码*/
+    /**
+     * 根据用户名获取密码
+     *
+     * @param username
+     * @return
+     */
     public String getPasswd(String username);
 
-    /*根据ID获取密码*/
+    /**
+     * 根据ID获取密码
+     *
+     * @param uid
+     * @return
+     */
     public String getPasswdById(String uid);
 
-    /*获取是否为第一次登陆*/
+    /**
+     * 获取是否为第一次登陆
+     *
+     * @param uid
+     * @return
+     */
     public boolean isFirstLogin(String uid);
 
-    /*封装用户信息*/
+    /**
+     * 封装用户信息
+     *
+     * @param username
+     * @return
+     */
     public User getUserEntity(String username);
 
-    /*修改密码*/
+    /**
+     * 修改密码
+     *
+     * @param map
+     */
     public void setUserPasswd(Map<String, String> map);
 
-    /*设置登陆标记*/
+    /**
+     * 设置登陆标记
+     *
+     * @param isfirstlogin
+     */
     public void setFirstLogin(Map<String, Object> isfirstlogin);
 
+    /**
+     * @param uid
+     * @return
+     */
     public User getUserEntityByID(String uid);
 
+    /**
+     * @return
+     */
     public List<User> getUserList();
 
+    /**
+     * @param userOpenID
+     * @return
+     */
     public User getUserEntityByOpenID(String userOpenID);
 
+    /**
+     * @param user
+     */
     public void insertQQIDByUID(User user);
 }
