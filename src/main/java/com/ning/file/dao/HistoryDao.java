181877f18d7ec207b99833aa7cd4bd809cfe45ad
376512291@qq.com <376512291@qq.com>
2018-04-19 09:15:08 +0800

    filedao

--- src/main/java/com/ning/file/dao/HistoryDao.java
+++ src/main/java/com/ning/file/dao/HistoryDao.java
@@ -6,22 +6,51 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Created by wangn on 2017/5/22.
+ * @author wangn
+ * @date 2017/5/22
  */
 public interface HistoryDao {
+    /**
+     * @param history
+     */
     public void insertDataByEntity(History history);
 
+    /**
+     * @param huid
+     * @return
+     */
     public List<History> getUpListByUID(String huid);
 
+    /**
+     * @param delHid
+     */
     public void delEntityByHID(String delHid);
 
+    /**
+     * @param hid
+     * @return
+     */
     public History getEntityByHID(String hid);
 
+    /**
+     * @param hoidhuid
+     * @return
+     */
     public History findHuidExists(Map<String, Object> hoidhuid);
 
+    /**
+     * @param history
+     */
     public void upHistoryData(History history);
 
+    /**
+     * @param hoid
+     * @return
+     */
     public List<History> findFileListByHoid(Integer hoid);
 
+    /**
+     * @param hoid
+     */
     public void delEntityByHOID(Integer hoid);
 }
